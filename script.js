b6506596569723938e258f83cd43d42760c91ce3
EZEQUIEL TEIXEIRA SANTOS <57824564851@info.local>
2024-05-29 08:57:03 -0300

    feat: Formulário cadastrado

--- script.js
+++ script.js
@@ -41,3 +41,52 @@ onresize = () => {
 
     iconeX.style.display = "inline"
 }
+
+
+// Receber pedidos do formulário
+
+function solicitarOrcamento(event) {
+    // Pegar os valores dos inputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorDescricao = document.getElementById("campo-texto").value
+    
+    // Organizar os valores em um objeto
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+
+    // Enviar a requisição para a API
+
+    //metodo HTTP POST - Criar/Create -> Cadastrar um novo registro (solicitacao)
+    fetch("http://localhost:3000/pedidos", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+
+        // CASO SUCESSO
+        .then(resposta => {
+            console.log(resposta)
+
+            // Limpar os inputs
+            document.querySelector("#form").reset()
+
+            // Mostrar um alert de sucesso
+            alert("Solicitação enviada com sucesso! 😉")
+        })
+
+        // CASO ERRO
+        .catch(erro => {
+            // Mostar um alert com erro
+            console.log(erro)
+            alert("Erro na requisição! 🫤")
+        })
+        
+        // previni que o formulário regarregue a página ao enviar
+        event.preventDefault()
+}
